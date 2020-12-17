50ae0f8c2941cf1ac77e698961ca4b0351bdc22e
risanomura110 <aiueo110boy@gmail.com>
2020-12-17 09:52:31 +0900

    固定メニュー

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,23 @@
+// jQuery(function($){
+//   // DOM Readyになったら処理する
+//   $(function() {
+//     // CSSのnavbar-wrapperクラス内を固定メニューにする
+//     $(".nav").stickUp();
+//   });
+// });
+
+// jQuery(function($){
+//   $(function() {
+//     $(".nav").stickUp();
+//   });
+// });
+
 jQuery(function($){
-  // DOM Readyになったら処理する
   $(function() {
-    // CSSのnavbar-wrapperクラス内を固定メニューにする
-    $(".nav").stickUp();
+  var w = jQuery(window).width();
+  var x = 481;
+  if (w >= x) {
+    $(".nav-wrapper").stickUp();
+  }
   });
 });
